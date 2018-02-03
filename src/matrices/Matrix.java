56447b6811dae7f3ac898d583ffa5c9b0674c26d
Housing <Housing@10.38.16.194>
2018-02-03 00:17:38 -0500

    committing Matrix class:
    added constructor,
    initRowsCols method,
    getRows method
    getCols method
    isCompatible method
    ijProduct method

--- src/matrices/Matrix.java
+++ src/matrices/Matrix.java
@@ -1,5 +1,40 @@
 package matrices;
 
 public class Matrix {
+	
+	private int[][] array;
+	private int numberOfRows;
+	private int numberOfColumns;
+	
+	public Matrix(int[][] array) {
+		this.array = array;
+		initRowsCols();
+	}
 
+	private void initRowsCols() {
+		numberOfRows = array.length;
+		numberOfColumns = array[0].length;
+	}	
+	
+	public int getRows() {
+		return numberOfRows;
+	}
+	
+	public int getCols() {
+		return numberOfColumns;
+	}
+	
+	public boolean isCompatible(Matrix matrix) {
+		return numberOfColumns == matrix.getRows();
+	}
+	
+	public int ijProduct(Matrix matrix, int row, int column) {
+		 int[] rowArray = array[row];
+		 int sum = 0;
+		 for (int i = 0; i < rowArray.length; i++) {
+			 sum += rowArray[i] * matrix.array[i][column];
+		 }
+		 return sum;
+	}
+	
 }
